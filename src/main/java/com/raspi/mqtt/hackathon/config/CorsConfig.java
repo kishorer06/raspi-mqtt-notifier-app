ed1e6b658d03a331d5ea559cb3c5092b8f44b977
GUJJA <kg811t@att.com>
2023-05-04 11:07:27 -0400

    cors changes

---
+++ src/main/java/com/raspi/mqtt/hackathon/config/CorsConfig.java
@@ -0,0 +1,16 @@
+package com.raspi.mqtt.hackathon.config;
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class CorsConfig {
+    @Bean
+    public FilterRegistrationBean<CorsFilter> corsFilter() {
+        FilterRegistrationBean<CorsFilter> registrationBean = new FilterRegistrationBean<>();
+        registrationBean.setFilter(new CorsFilter());
+        registrationBean.addUrlPatterns("/*");
+        return registrationBean;
+    }
+}
